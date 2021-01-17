ece51a5c9ab1c022f337247fbe3b0741c0569f62
dk <dk@g.com>
2021-01-17 21:21:25 +0100

    java 8 - e

--- Tescik/test/tescik/TescikTest.java
+++ Tescik/test/tescik/TescikTest.java
@@ -41,7 +41,7 @@ public class TescikTest {
     public void testVersion() {
 	final boolean JAVA_7_OR_EARLIER = System.getProperty("java.specification.version").compareTo("1.8") < 0;
 	System.out.println(System.getProperty("java.specification.version"));
-	//assertTrue(JAVA_7_OR_EARLIER);
+	assertTrue(JAVA_7_OR_EARLIER);
     }
 
     /**
@@ -59,7 +59,7 @@ public class TescikTest {
     public void testVersion8() {
 	final boolean JAVA_8_OR_EARLIER = System.getProperty("java.specification.version").compareTo("1.8") < 0;
 	System.out.println(System.getProperty("java.specification.version"));
-	//assertTrue(JAVA_8_OR_EARLIER);
+	assertTrue(JAVA_8_OR_EARLIER);
     }
     
 }
